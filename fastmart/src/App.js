7f643a9ac213730082a6866e4abe6a556db7ca46
Haris <harisalimughal077@gmail.com>
2024-10-29 11:45:55 +0500

    Add routing functionality using react-router-dom
    Implement routing in the application with react-router-dom
    
    - Added BrowserRouter to manage routing.
    - Set up routes for Login and Signup components.
    - Updated App component to conditionally render based on route.
    
    [Ticket: X]

--- fastmart/src/App.js
+++ fastmart/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'; // Import Routes and Route
+import Login from './components/Login';
+import Signup from './components/SignUp';
 import Navbar from './components/Navbar';
 import CategorySection from './components/CategorySection';
 import RecentAdsSection from './components/RecentAdsSection';
@@ -7,8 +10,13 @@ function App() {
   return (
     <div className="App font-acad flex flex-col gap-16">
       <Navbar />
-      <CategorySection />
-      <RecentAdsSection />
+      <Routes>
+        <Route path="/" element={<CategorySection />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/categories" element={<CategorySection />} />
+        <Route path="/recent-ads" element={<RecentAdsSection />} />
+      </Routes>
     </div>
   );
 }
