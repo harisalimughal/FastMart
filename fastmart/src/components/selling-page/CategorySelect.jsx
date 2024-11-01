36cbbc05f80cf1cef67c06db8bf0795a8bacf87d
Haris Sohail <harissohail.cs@gmail.com>
2024-11-01 20:32:49 +0500

    Change size of category select icons

--- fastmart/src/components/selling-page/CategorySelect.jsx
+++ fastmart/src/components/selling-page/CategorySelect.jsx
@@ -12,38 +12,38 @@ import sports from '../../assets/categories/sports.png'
 
 function CategorySelect() {
   return (
-      <div className='flex justify-between gap-4 md:gap-0 overflow-x-auto custom-scrollbar'>
+      <div className='flex justify-between gap-4 md:gap-0 overflow-x-auto custom-scrollbar w-4/5'>
 
-          <button className='bg-gray-200 rounded-full p-2 md:p-5 border-2 hover:border-black transition duration-300'>
-              <img src={animal} className='w-14 min-w-10 min-h-10' alt='animal'></img>
+          <button className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'>
+              <img src={animal} className='w-10 min-w-10 min-h-10' alt='animal'></img>
           </button>
 
-          <button className='bg-gray-200 rounded-full p-2 md:p-5 border-2 hover:border-black transition duration-300'>
-              <img src={book} className='w-14 min-w-10 min-h-10' alt='book'></img>
+          <button className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'>
+              <img src={book} className='w-10 min-w-10 min-h-10' alt='book'></img>
           </button>
 
-          <button className='bg-gray-200 rounded-full p-2 md:p-5 border-2 hover:border-black transition duration-300'>
-              <img src={car} className='w-14 min-w-10 min-h-10' alt='car'></img>
+          <button className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'>
+              <img src={car} className='w-10 min-w-10 min-h-10' alt='car'></img>
           </button>
 
-          <button className='bg-gray-200 rounded-full p-2 md:p-5 border-2 hover:border-black transition duration-300'>
-              <img src={clothing} className='w-14 min-w-10 min-h-10' alt='clothing'></img>
+          <button className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'>
+              <img src={clothing} className='w-10 min-w-10 min-h-10' alt='clothing'></img>
           </button>
 
-          <button className='bg-gray-200 rounded-full p-2 md:p-5 border-2 hover:border-black transition duration-300'>
-              <img src={electronics} className='w-14 min-w-10 min-h-10' alt='electronics'></img>
+          <button className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'>
+              <img src={electronics} className='w-10 min-w-10 min-h-10' alt='electronics'></img>
           </button>
 
-          <button className='bg-gray-200 rounded-full p-2 md:p-5 border-2 hover:border-black transition duration-300'>
-              <img src={gaming} className='w-14 min-w-10 min-h-10' alt='gaming'></img>
+          <button className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'>
+              <img src={gaming} className='w-10 min-w-10 min-h-10' alt='gaming'></img>
           </button>
 
-          <button className='bg-gray-200 rounded-full p-2 md:p-5 border-2 hover:border-black transition duration-300'>
-              <img src={sports} className='w-14 min-w-10 min-h-10' alt='sports'></img>
+          <button className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'>
+              <img src={sports} className='w-10 min-w-10 min-h-10' alt='sports'></img>
           </button>
 
-          <button className='bg-gray-200 rounded-full p-2 md:p-5 border-2 hover:border-black transition duration-300'>
-              <img src={others} className='w-14 min-w-10 min-h-10' alt='others'></img>
+          <button className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'>
+              <img src={others} className='w-10 min-w-10 min-h-10' alt='others'></img>
           </button>
 
 
