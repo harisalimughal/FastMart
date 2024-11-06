4480530a3d83d9e40facd6520fd91beeac14b604
Haris Sohail <harissohail.cs@gmail.com>
2024-11-06 21:56:55 +0500

    Handle category select change
    1. Handle the CSS for the category select change
    2. Handle the state change for the category select change

--- fastmart/src/components/selling-page/CategorySelect.jsx
+++ fastmart/src/components/selling-page/CategorySelect.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Import category images
 import animal from '../../assets/categories/animal.png'
@@ -11,66 +11,79 @@ import others from '../../assets/categories/others.png'
 import sports from '../../assets/categories/sports.png'
 
 function CategorySelect({ onCategoryChange }) {
+    const [selectedCategory, setSelectedCategory] = useState('');
+
+    // function to handle category click
+    const handleCategoryClick = (category) => {
+        const newCategory = selectedCategory === category ? '' : category; // unselect category if already selected
+        setSelectedCategory(newCategory);
+        onCategoryChange(newCategory);
+    };
+
+    // function to get button class according to category selection state
+    const getButtonClass = (category) => {
+        return `bg-gray-200 rounded-full p-2 md:p-3 border-2 transition duration-300 ${
+            selectedCategory === category ? 'border-black' : 'hover:border-black'
+        }`;
+    };
+
     return (
         <div className='flex justify-between gap-4 md:gap-0 overflow-x-auto custom-scrollbar w-4/5'>
-
             <button
-                onClick={() => onCategoryChange('animal')}
-                className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'
+                onClick={() => handleCategoryClick('animal')}
+                className={getButtonClass('animal')}
             >
                 <img src={animal} className='w-10 min-w-10 min-h-10' alt='animal'></img>
             </button>
 
             <button
-                onClick={() => onCategoryChange('book')}
-                className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'
+                onClick={() => handleCategoryClick('book')}
+                className={getButtonClass('book')}
             >
                 <img src={book} className='w-10 min-w-10 min-h-10' alt='book'></img>
             </button>
 
             <button
-                onClick={() => onCategoryChange('car')}
-                className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'
+                onClick={() => handleCategoryClick('car')}
+                className={getButtonClass('car')}
             >
                 <img src={car} className='w-10 min-w-10 min-h-10' alt='car'></img>
             </button>
 
             <button
-                onClick={() => onCategoryChange('clothing')}
-                className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'
+                onClick={() => handleCategoryClick('clothing')}
+                className={getButtonClass('clothing')}
             >
                 <img src={clothing} className='w-10 min-w-10 min-h-10' alt='clothing'></img>
             </button>
 
             <button
-                onClick={() => onCategoryChange('electronics')}
-                className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'
+                onClick={() => handleCategoryClick('electronics')}
+                className={getButtonClass('electronics')}
             >
                 <img src={electronics} className='w-10 min-w-10 min-h-10' alt='electronics'></img>
             </button>
 
             <button
-                onClick={() => onCategoryChange('gaming')}
-                className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'
+                onClick={() => handleCategoryClick('gaming')}
+                className={getButtonClass('gaming')}
             >
                 <img src={gaming} className='w-10 min-w-10 min-h-10' alt='gaming'></img>
             </button>
 
             <button
-                onClick={() => onCategoryChange('sports')}
-                className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'
+                onClick={() => handleCategoryClick('sports')}
+                className={getButtonClass('sports')}
             >
                 <img src={sports} className='w-10 min-w-10 min-h-10' alt='sports'></img>
             </button>
 
             <button
-                onClick={() => onCategoryChange('others')}
-                className='bg-gray-200 rounded-full p-2 md:p-3 border-2 hover:border-black transition duration-300'
+                onClick={() => handleCategoryClick('others')}
+                className={getButtonClass('others')}
             >
                 <img src={others} className='w-10 min-w-10 min-h-10' alt='others'></img>
             </button>
-
-
         </div>
     )
 }
