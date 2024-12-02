9784a2ecbd725079307300d88fbf86c49fc2938c
Haris Sohail <harissohail.cs@gmail.com>
2024-12-02 15:22:35 +0500

    Add design for PostAd component for mobile screens

--- fastmart/src/components/selling-page/PostAd.jsx
+++ fastmart/src/components/selling-page/PostAd.jsx
@@ -36,12 +36,12 @@ function PostAd() {
             <Navbar />
 
             {/* Money Emoji */}
-            <div className='w-48 mx-auto absolute right-44 top-36 -z-10 opacity-50'>
+            <div className='w-28 md:w-48 mx-auto absolute md:right-[9vw] md:top-[12vh] right-0 top-[45vw] -z-10 opacity-50'>
                 <img src={moneyEmoji} alt='money-emoji'></img>
             </div>
 
-            {/* Main Content */}
-            <div className='flex flex-col gap-5'>
+            {/* Main Content - Desktop */}
+            <div className='md:flex flex-col gap-5 hidden'>
                 {/* Header */}
                 <h1 className='w-fit mx-auto text-4xl font-bold'>
                     Post Your Ad
@@ -165,6 +165,138 @@ function PostAd() {
                         <button
                             type='submit'
                             className='flex justify-center items-center text-white bg-palletePurple font-bold text-2xl py-2 px-10 rounded-lg mx-auto hover:shadow-xl transition duration-300'
+                        >
+                            Post
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+            {/* Main Content - Mobile */}
+            <div className='flex flex-col gap-5 md:hidden'>
+                {/* Header */}
+                <h1 className='w-fit mx-auto text-2xl font-bold'>
+                    Post Your Ad
+                </h1>
+
+                {/* Form */}
+                <div className='shadow-xl w-11/12 mx-auto p-5 rounded-2xl'>
+                    <form
+                        onSubmit={handleSubmit}
+                        className='flex flex-col gap-10'
+                    >
+                        {/* Category */}
+                        <div className='flex flex-col gap-4'>
+                            <label className='text-lg font-bold'>Category</label>
+                            <CategorySelect onCategoryChange={handleCategoryChange} />
+                        </div>
+
+                        {/* Upload Images */}
+                        <div className='flex flex-col gap-4'>
+                            <label className='text-lg font-bold'>Upload Images</label>
+                            <div className='flex gap-5'>
+                                <UploadAdImage />
+                                <UploadAdImage />
+                                <UploadAdImage />
+                            </div>
+                        </div>
+
+                        {/* Condition */}
+                        <div className='flex flex-col gap-4'>
+                            <label className='text-lg font-bold'>Condition</label>
+                            <ConditionSelect onConditionChange={handleConditionChange} />
+                        </div>
+
+                        {/* Title */}
+                        <div className='flex flex-col gap-4'>
+                            <label className='text-lg font-bold'>Ad Title</label>
+                            <input
+                                type='text'
+                                className='border-2 border-black p-2 rounded-lg text-md'
+                                placeholder='Describe your items in a few words (e.g. brand, model, type, age) ...'
+                                onInput={(e) => setTitle(e.target.value)}
+                                value={title}
+                            />
+                        </div>
+
+                        {/* Description */}
+                        <div className='flex flex-col gap-4'>
+                            <label className='text-lg font-bold'>Description</label>
+                            <textarea
+                                className='border-2 border-black p-2 rounded-lg text-md'
+                                placeholder='Include condition, features, and reason for selling ...'
+                                rows={5}
+                                onInput={(e) => setDescription(e.target.value)}
+                                value={description}
+                            />
+                        </div>
+
+                        {/* Price */}
+                        <div className='flex flex-col gap-4'>
+                            <label className='text-lg font-bold'>Price</label>
+                            <div className='relative flex items-center'>
+                                <span className='absolute left-3'>Rs.</span>
+                                <input
+                                    type='number'
+                                    min={0}
+                                    step={1}
+                                    className='pl-10 pr-3 py-2 border-2 border-black rounded-lg text-md'
+                                    placeholder='Enter your price ...'
+                                    onInput={(e) => {
+                                        setPrice(e.target.value);
+                                    }}
+                                    value={price}
+                                />
+                            </div>
+                        </div>
+
+                        {/* Campus Location */}
+                        <div className='flex flex-col gap-4'>
+                            <label className='text-lg font-bold'>Campus Location</label>
+                            <select
+                                onChange={(e) => setCampus(e.target.value)}
+                                className='py-2 pl-3 pr-20 bg-white w-fit border-2 border-black rounded-lg'
+                            >
+                                <option value=''>Select Campus</option>
+                                <option value='Islamabad'>Islamabad</option>
+                                <option value='Karachi'>Karachi</option>
+                                <option value='Lahore'>Lahore</option>
+                                <option value='Peshawar'>Peshawar</option>
+                                <option value='Chiniot-Faislabad'>Chiniot-Faislabad</option>
+                            </select>
+                        </div>
+
+                        {/* Name */}
+                        <div className='flex flex-col gap-4'>
+                            <label className='text-lg font-bold'>Name</label>
+                            <input
+                                type='text'
+                                className='border-2 border-black p-2 pr-10 rounded-lg text-md w-fit'
+                                placeholder='Enter your name ...'
+                                onInput={(e) => setName(e.target.value)}
+                                value={name}
+                            />
+                        </div>
+
+                        {/* Contact */}
+                        <div className='flex flex-col gap-4'>
+                            <label className='text-lg font-bold'>Contact</label>
+                            <div className='relative flex items-center'>
+                                <span className='absolute left-3'>
+                                    +92 |
+                                </span>
+                                <input
+                                    type='text'
+                                    className='border-2 border-black py-2 pl-14 rounded-lg text-md w-fit'
+                                    placeholder='320-1234567'
+                                />
+                            </div>
+                        </div>
+
+                        {/* Post */}
+                        <button
+                            type='submit'
+                            className='flex justify-center items-center text-white bg-palletePurple font-bold text-lg py-2 px-10 rounded-lg mx-auto hover:shadow-xl transition duration-300'
                         >
                             Post
                         </button>
--- fastmart/src/components/selling-page/UploadAdImage.jsx
+++ fastmart/src/components/selling-page/UploadAdImage.jsx
@@ -4,8 +4,8 @@ import plusIcon from '../../assets/others/plus.png'
 
 function UploadAdImage() {
     return (
-        <div className='px-7 py-5 bg-gray-100 w-fit border-2 border-black rounded-xl text-2xl hover:cursor-pointer'>
-            <img src={plusIcon} alt='plus-icon' className='w-5 h-5'></img>
+        <div className='px-6 py-4 md:px-7 md:py-5 bg-gray-100 w-fit border-2 border-black rounded-xl text-2xl hover:cursor-pointer'>
+            <img src={plusIcon} alt='plus-icon' className='w-3 md:w-5 md:h-5'></img>
         </div>
     )
 }
