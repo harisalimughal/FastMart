24b226fd36190a6b35b1a99e5d955afae6b4bb17
haris-sohail <harissohail.cs@gmail.com>
2025-05-22 17:06:23 +0500

    Complete the design of ad details page

--- frontend/src/components/ad-details/AdDetails.jsx
+++ frontend/src/components/ad-details/AdDetails.jsx
@@ -3,7 +3,8 @@ import { useParams } from 'react-router-dom';
 import Navbar from '../NavBar';
 import ImageCarousel from './ImageCarousel';
 import AdDetailsSubSection from './AdDetailsSubSection';
-import { LuMapPin } from 'react-icons/lu';
+import { LuMapPin, LuShare2 } from 'react-icons/lu';
+import { FaRegHeart } from "react-icons/fa6";
 
 function AdDetails() {
     const { id } = useParams();
@@ -18,6 +19,11 @@ function AdDetails() {
     });
     const [location, setLocation] = useState("Islamabad");
     const [condition, setCondition] = useState("Used");
+    const [price, setPrice] = useState("100000");
+    const [title, setTitle] = useState("Corolla Altis 2023, White Color");
+    const [dateListed, setDateListed] = useState("2023-10-01");
+    const [description, setDescription] = useState("This is a sample description of the ad. It provides details about the item being sold.");
+
 
     return (
         <div className='flex flex-col md:gap-10 gap-4 pb-10 w-full'>
@@ -54,7 +60,44 @@ function AdDetails() {
                                 descriptionLine1={condition}
                             />
                         </div>
+
+                        {/* Ad Price & Title */}
+                        <div className='col-span-4 flex flex-col p-4 bg-[#efefef] rounded-lg gap-2'>
+                            <div className='flex items-center justify-between'>
+                                <p className='text-2xl font-bold'>
+                                    Rs. {price}
+                                </p>
+
+                                {/* Share and Like Button */}
+                                <div className='flex gap-4'>
+                                    <button>
+                                        <LuShare2 className='text-xl' />
+                                    </button>
+
+                                    <button>
+                                        <FaRegHeart className='text-xl' />
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className='flex items-center justify-between'>
+                                <p className='text-xl'>
+                                    {title}
+                                </p>
+
+                                <p className='text-gray-500'>
+                                    {new Date(dateListed).toLocaleDateString()}
+                                </p>
+                            </div>
+                        </div>
+
+                        {/* Ad Description */}
+                        <div className='col-span-4 p-4 bg-[#efefef] rounded-lg flex flex-col gap-2'>
+                            <h2 className='text-2xl font-bold'>Seller's Description</h2>
+                            <p className='text-xl'>{description}</p>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
